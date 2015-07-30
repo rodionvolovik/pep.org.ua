7e315c8538d69aa966ef55902fb61abce3a915b8
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2015-07-30 02:04:54 +0200

    load_peps command now working directly with google spreadsheet, import of person2company links fixed

--- pepdb/core/utils.py
+++ pepdb/core/utils.py
@@ -1,11 +1,18 @@
 import re
 import os.path
 import requests
+import urllib2
+import httplib2
+import json
+from oauth2client.client import SignedJwtAssertionCredentials
+
 from string import capwords
 from datetime import datetime
 
 from dateutil import parser
 from rfc6266 import parse_requests_response
+
+from django.conf import settings
 
 
 def expand_gdrive_download_url(url):
@@ -54,3 +61,29 @@ def parse_date(s):
         return parser.parse(s, default=datetime(1970, 1, 1)).date()
     except ValueError:
         return None
+
+
+class Client(object):
+    def __init__(self, auth_key=settings.GAUTH_CREDENTIALS):
+        super(Client, self).__init__()
+        self.auth_key = auth_key
+
+    def get_auth_token(self):
+        scope = ['https://spreadsheets.google.com/feeds']
+        credentials = SignedJwtAssertionCredentials(
+            self.auth_key['client_email'], self.auth_key['private_key'], scope)
+
+        credentials.refresh(httplib2.Http())
+        return credentials.token_response["access_token"]
+
+    def download(self, spreadsheet=settings.SPREADSHEET_ID, gid=0,
+                 format="csv"):
+        url_format = ("https://spreadsheets.google.com/feeds/download/"
+                      "spreadsheets/Export?key=%s&exportFormat=%s&gid=%i")
+        headers = {
+            "Authorization": "Bearer " + self.get_auth_token()
+        }
+        req = urllib2.Request(url_format % (spreadsheet, format, gid),
+                              headers=headers)
+
+        return urllib2.urlopen(req)
