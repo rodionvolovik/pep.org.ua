14e7ffa986930bb16cecd59c46af2a3c4166a4e2
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2015-11-28 02:07:48 +0200

    Missing migration added

---
+++ pepdb/core/migrations/0056_auto_20151128_0206.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0055_auto_20151115_1810'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='feedbackmessage',
+            name='contacts',
+            field=models.TextField(max_length=512, verbose_name='\u041a\u043e\u043d\u0442\u0430\u043a\u0442\u0438', blank=True),
+        ),
+    ]
