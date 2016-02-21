968bb36d3b8dd7dc94af579085b81df570129e87
ArialBlack <andreyturik@gmail.com>
2016-02-21 23:50:53 +0200

    fixes

--- pepdb/pepdb/urls.py
+++ pepdb/pepdb/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import include, patterns, url
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
@@ -60,3 +60,9 @@ urlpatterns += [
     url(r'^cms/', include(wagtailadmin_urls)),
     url(r'^wg_search/', include(wagtailsearch_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
