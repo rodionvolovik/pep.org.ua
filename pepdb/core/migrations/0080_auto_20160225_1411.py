6c85c29cf6cfd6e4db94be4d8e14ef6b9deda473
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2016-02-25 14:14:01 +0200

    Fixing wiki fields

---
+++ pepdb/core/migrations/0080_auto_20160225_1411.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+def fix_markdown_fields(apps, schema_editor):
+    Person = apps.get_model("core", "Person")
+
+    def fix(s):
+        if s == "<p>None</p>":
+            return ""
+        return s
+
+    for p in Person.objects.all():
+        p.wiki_en = fix(p.wiki_uk)
+
+        p.reputation_assets_uk = fix(p.reputation_assets_uk)
+        p.reputation_sanctions_uk = fix(p.reputation_sanctions_uk)
+        p.reputation_crimes_uk = fix(p.reputation_crimes_uk)
+        p.reputation_manhunt_uk = fix(p.reputation_manhunt_uk)
+        p.reputation_convictions_uk = fix(p.reputation_convictions_uk)
+        p.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0079_auto_20160225_1332'),
+    ]
+
+    operations = [
+        migrations.RunPython(fix_markdown_fields),
+    ]
