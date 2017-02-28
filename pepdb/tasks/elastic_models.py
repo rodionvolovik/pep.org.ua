2c1400812a3c9bdcbdab44e3f5ee8ba7d7abe6e1
Dmitry Chaplinsky <chaplinsky.dmitry@gmail.com>
2017-02-28 19:32:42 +0200

    Loading EDR data into search index

---
+++ pepdb/tasks/elastic_models.py
@@ -0,0 +1,16 @@
+from elasticsearch_dsl import Keyword, Text, DocType
+
+
+class EDRPOU(DocType):
+    """EDRPOU."""
+
+    edrpou = Keyword(index=True)
+    location = Text(index=True, analyzer='ukrainian')
+    company_profile = Text(index=True, analyzer='ukrainian')
+    head = Text(index=True, analyzer='ukrainian')
+    name = Text(index=True, analyzer='ukrainian')
+    short_name = Text(index=True, analyzer='ukrainian')
+    status = Keyword(index=True)
+
+    class Meta:
+        index = 'edrpou'
---
+++ pepdb/tasks/management/commands/load_companies.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
+from elasticsearch_dsl import Index
+from elasticsearch_dsl.connections import connections
+from elasticsearch.helpers import bulk
+from unicodecsv import DictReader
+
+from tasks.elastic_models import EDRPOU
+
+
+class Command(BaseCommand):
+    help = ('Loads CSV with data from registry of companies of Ukraine into '
+            'elastic index for further matching with companies in DB')
+
+    args = '<file_path>'
+
+    def handle(self, *args, **options):
+        try:
+            file_path = args[0]
+        except IndexError:
+            raise CommandError(
+                'First argument must be a CSV file with companies')
+
+        Index(EDRPOU._doc_type.index).delete(ignore=404)
+        EDRPOU.init()
+        es = connections.get_connection()
+
+        with open(file_path, "r") as fp:
+            dr = DictReader(fp, encoding="utf-8")
+            portion = []
+
+            for i, row in enumerate(dr):
+                portion.append(EDRPOU(
+                    edrpou=row[u"Код_ЄДРПОУ"].lstrip("0"),
+                    location=row[u"Місцезнаходження"],
+                    company_profile=row[u"Основний_вид_діяльності"],
+                    head=row[u"ПІБ_керівника"],
+                    name=row[u"Найменування"],
+                    short_name=row[u"Скорочена_назва"],
+                    status=row[u"Стан"],
+                ).to_dict(True))
+
+                if len(portion) >= 10000:
+                    bulk(es, portion)
+
+                    self.stdout.write(
+                        'Loaded {} registry companies to search index'.format(i + 1))
+
+                    portion = []
+
+            bulk(es, portion)        
