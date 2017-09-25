54e7d1fbe0578dfdd4e9c2c461205ac17d6f0fe9
Dmitry Chaplinsky <chaplinsky.dmitry@gmail.com>
2017-09-26 02:28:27 +0300

    Export for foreign companies mentioned in declarations but not yet added to our db

---
+++ pepdb/tasks/management/commands/export_foreign_companies.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import json
+import xlsxwriter
+from django.core.management.base import BaseCommand
+from tasks.models import BeneficiariesMatching
+
+
+class Command(BaseCommand):
+    help = ('Exports the list of foreign companies from declarations of PEPs '
+            'which aren\'t yet in DB to an excel file for further processing '
+            'and reconciliation with the registry')
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'target_file',
+            help='Excel file to export to',
+        )
+
+    def handle(self, *args, **options):
+        tasks = BeneficiariesMatching.objects.filter(status="n")
+
+        keys = [
+            "owner_name",
+            "company_name_declaration",
+            "company_name_en",
+            "zip",
+            "city",
+            "street",
+            "appt",
+            "country",
+            "company_code",
+            "notes",
+            "status",
+            "company_name_orig",
+            "link",
+            "founder_1",
+            "founder_2",
+            "founder_3",
+            "founder_4",
+            "founder_5",
+            "founder_6",
+            "founder_7"
+        ]
+
+        workbook = xlsxwriter.Workbook(options["target_file"])
+        ws = workbook.add_worksheet()
+
+        for i, f in enumerate(keys):
+            ws.write(0, i, f)
+
+        row = 1
+        for t in tasks:
+            base_res = {
+                "owner_name": t.person_json["full_name"]
+            }
+
+            for company in t.pep_company_information:
+                res = base_res.copy()
+                res["company_name_declaration"] = company["company_name"]
+                res["company_name_en"] = company["en_name"] or ""
+                res["country"] = company["country"]
+                res["zip"] = company["address"] or ""
+                res["company_code"] = company["beneficial_owner_company_code"]
+
+                for i, f in enumerate(keys):
+                    ws.write(row, i, res.get(f, ""))
+
+                row += 1
+
+        workbook.close()
