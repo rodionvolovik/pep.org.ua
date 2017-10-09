a67d72cbb1d5ad660ddaf39a5b9b9b8f8dfc13e2
Dmitry Chaplinsky <chaplinsky.dmitry@gmail.com>
2017-10-10 00:39:44 +0300

    Script to import en translations from CSV file

---
+++ pepdb/core/management/commands/import_en_translations.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.core.management.base import BaseCommand
+from core.models import Ua2EnDictionary
+from unicodecsv import reader
+from django.db.models import Q
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            'file_path',
+            help='CSV file to import translations',
+        )
+        parser.add_argument(
+            '--replace',
+            default=False,
+            action="store_true",
+            help='Replace existing non-empty translations',
+        )
+
+    def handle(self, *args, **options):
+        file_path = options['file_path']
+        successful = 0
+        failed = 0
+
+        with open(file_path, "r") as fp:
+            r = reader(fp)
+
+            for term, trans in r:
+                term = term.strip()
+                trans = trans.strip()
+
+                if term and trans:
+                    try:
+                        obj = Ua2EnDictionary.objects.get(term=term)
+
+                        if obj.translation:
+                            if obj.translation == trans:
+                                continue
+
+                            if options["replace"]:
+                                obj.translation = trans
+                                obj.save()
+                                successful += 1
+                            else:
+                                self.stdout.write(
+                                    "Not overwritting existing translation %s for term %s with %s" % (
+                                        term, obj.translation, trans))
+                                failed += 1
+                                continue
+
+                        obj.translation = trans
+                        obj.save()
+                        successful += 1
+                    except Ua2EnDictionary.DoesNotExist:
+                        self.stderr.write("Cannot find term %s in db" % term)
+                        failed += 1
+                    except Ua2EnDictionary.MultipleObjectsReturned:
+                        self.stderr.write("More than one result for term %s in db" % term)
+                        failed += 1
+
+            self.stdout.write("Import is done, successful: %s, failed: %s" % (successful, failed))
