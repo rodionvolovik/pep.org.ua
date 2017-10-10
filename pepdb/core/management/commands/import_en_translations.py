7b6fdcf2c4e4e4979617075df40a5971e999585b
Dmitry Chaplinsky <chaplinsky.dmitry@gmail.com>
2017-10-10 06:13:31 +0300

    Fixing search for translations with shortage of spaces

--- pepdb/core/management/commands/import_en_translations.py
+++ pepdb/core/management/commands/import_en_translations.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.core.management.base import BaseCommand
 from core.models import Ua2EnDictionary
 from unicodecsv import reader
-from django.db.models import Q
+from django.db.models import F, Func, Value
 
 
 class Command(BaseCommand):
@@ -34,30 +34,43 @@ class Command(BaseCommand):
                 if term and trans:
                     try:
                         obj = Ua2EnDictionary.objects.get(term=term)
-
-                        if obj.translation:
-                            if obj.translation == trans:
-                                continue
-
-                            if options["replace"]:
-                                obj.translation = trans
-                                obj.save()
-                                successful += 1
-                            else:
-                                self.stdout.write(
-                                    "Not overwritting existing translation %s for term %s with %s" % (
-                                        term, obj.translation, trans))
-                                failed += 1
-                                continue
-
-                        obj.translation = trans
-                        obj.save()
-                        successful += 1
                     except Ua2EnDictionary.DoesNotExist:
-                        self.stderr.write("Cannot find term %s in db" % term)
-                        failed += 1
+                        self.stderr.write("Cannot find term %s in db, falling back to fuzzier search" % term)
+                        term = term.replace(" ", "")
+
+                        try:
+                            obj = Ua2EnDictionary.objects.annotate(
+                                term_key=Func(F('term'), Value(' '), Value(''), function='replace')).get(term_key=term)
+                        except Ua2EnDictionary.MultipleObjectsReturned:
+                            self.stderr.write("More than one result for term %s in db" % term)
+                            failed += 1
+                            continue
+                        except Ua2EnDictionary.DoesNotExist:
+                            self.stderr.write("Cannot find term %s in db, falling back to fuzzier search" % term)
+                            failed += 1
+                            continue
                     except Ua2EnDictionary.MultipleObjectsReturned:
                         self.stderr.write("More than one result for term %s in db" % term)
                         failed += 1
+                        continue
+
+                    if obj.translation:
+                        if obj.translation == trans:
+                            continue
+
+                        if options["replace"]:
+                            obj.translation = trans
+                            obj.save()
+                            successful += 1
+                        else:
+                            self.stdout.write(
+                                "Not overwritting existing translation %s for term %s with %s" % (
+                                    term, obj.translation, trans))
+                            failed += 1
+                            continue
+
+                    obj.translation = trans
+                    obj.save()
+                    successful += 1
 
             self.stdout.write("Import is done, successful: %s, failed: %s" % (successful, failed))
