9a8bfae0a310a0c10f0f36bc97da17d447fb3abc
fabricantes <monsieur.ruslan@gmail.com>
2018-10-27 14:50:40 +0300

    Create P2P. Some refactoring

--- pepdb/tasks/management/commands/apply_smida_report.py
+++ pepdb/tasks/management/commands/apply_smida_report.py
@@ -9,10 +9,9 @@ from django.core.management.base import BaseCommand
 
 from core.model.companies import Company
 from core.model.persons import Person
-from core.model.connections import Person2Company
+from core.model.connections import Person2Company, Person2Person
 from tasks.models import SMIDACandidate
 from dateutil.parser import parse as dt_parse
-from core.utils import render_date
 from django.utils.translation import activate
 from django.conf import settings
 
@@ -21,111 +20,151 @@ class Command(BaseCommand):
 
     help = "Add data from matches with wikidata to the PEP db"
 
-    wikimedia = "https://upload.wikimedia.org/wikipedia/commons/"
-
     def add_arguments(self, parser):
         parser.add_argument(
             "--real_run",
             default=False,
             action="store_true",
-            help="Add matched data to wiki articles",
+            help="Save data to database",
         )
 
     def handle(self, *args, **options):
         self.stdout.write("Starting matching job ...")
-
         activate(settings.LANGUAGE_CODE)
 
-        companies_created = 0
-
-        #####################################
-        ############ Companies ##############
-        #####################################
-        smida_candidates = SMIDACandidate.objects.filter(status="a")
+        # region Companies
+        self.stdout.write("Starting import Companies.")
 
         companies_dict = {}
+        created_cnt = 0
+        matched_cnt = 0
 
-        #for candidate in tqdm(smida_candidates.nocache().iterator(), total=smida_candidates.count()):
-        for candidate in smida_candidates.nocache().iterator():
+        smida_candidates = SMIDACandidate.objects.filter(status="a")
 
+        for candidate in tqdm(smida_candidates.nocache().iterator(),
+                              total=smida_candidates.count()):
             edrpou = unicode(candidate.smida_edrpou).rjust(8, "0")
 
+            if companies_dict.get(edrpou):
+                continue
+
             try:
-                company = companies_dict.get(edrpou) or Company.objects.nocache().get(edrpou=edrpou)
+                company = Company.objects.nocache().get(edrpou=edrpou)
+                matched_cnt += 1
             except Company.DoesNotExist:
                 company = Company(
                     edrpou=edrpou,
                     name_uk=candidate.smida_company_name.strip()
                 )
-                self.stdout.write("Created company {}".format(company))
-                companies_created += 1
+                tqdm.write("Created company {}".format(company))
+                created_cnt += 1
 
                 if options["real_run"]:
                     company.save()
 
             companies_dict[edrpou] = company
 
-
         self.stdout.write(
-            "Companies created: {}".format(companies_created)
-        )
+            "Finished import companies.\nFound existing: {}\nCreated new: {}."
+            .format(matched_cnt, created_cnt))
+        # endregion
 
-        #####################################
-        ############# Persons ###############
-        #####################################
-
-        smida_candidates = SMIDACandidate.objects.filter(status="a", smida_is_real_person=True)
+        # region Persons and P2C
+        self.stdout.write("Starting import Persons and Person2Company relations.")
+        smida_candidates = SMIDACandidate.objects.filter(status="a",
+                                                         smida_is_real_person=True)
 
         peps = self.all_peps_names()
-        created_peps = {}
+        persons_dict = {}
+        persons_created_total = 0
+        p2c_links_total = 0
 
-        associated = self.all_associated_names(peps)
-        created_assoc = {}
-
-        heads = self.company_heads_mapping()
-
-        for candidate in smida_candidates.nocache().iterator():     # for candidate in tqdm(smida_candidates.nocache().iterator(), total=smida_candidates.count()):
+        for candidate in tqdm(smida_candidates.nocache().iterator(),
+                              total=smida_candidates.count()):
             person_name = candidate.smida_parsed_name
             is_pep = person_name in peps
 
-            # if not is_pep:
-            #     continue
+            person = persons_dict.get(person_name)
+            if not person:
+                person = self.create_person(person_name, is_pep,
+                                            persons_dict, options["real_run"])
+                persons_created_total += 1
 
-            person = created_peps.get(person_name) \
-                  or created_assoc.get(person_name) \
-                  or self.create_person(candidate, is_pep, created_peps, created_assoc, options["real_run"])
+            if person:
+                edrpou = unicode(candidate.smida_edrpou).rjust(8, "0")
+                company = companies_dict.get(edrpou)
 
-            edrpou = unicode(candidate.smida_edrpou).rjust(8, "0")
-            company = companies_dict.get(edrpou)
+                p2c = Person2Company(from_person=person,
+                                     to_company=company,
+                                     relationship_type=candidate.smida_position,
+                                     is_employee=True)
 
-            if not company:
-                self.stderr.write("Can not find a company: {}".format(edrpou))
-                continue
+                p2c_links_total += 1
+                tqdm.write("Created P2C relation: id: {} ({}) <=> id: {} ({})"
+                           .format(person.id or "N/A",
+                                   person_name,
+                                   company.id or "N/A",
+                                   company.name_uk))
 
-            p2c = Person2Company(from_person=person,
-                                 to_company=company,
-                                 relationship_type=candidate.smida_position,
-                                 is_employee=True, #??? Check this is correct
-                                 )
-
-            if options["real_run"]:
-                p2c.save()
-
-        # Second iteration to create associated persons
-        for candidate in smida_candidates.nocache().iterator():
-            person_name = candidate.smida_parsed_name
-            is_pep = person_name in peps
-
-            if is_pep:
-                continue
-
-
+                if options["real_run"]:
+                    p2c.save()
 
         self.stdout.write(
-            "New PEPs count: {}".format(len(created_peps))
+            "Finished import Persons and Person2Company relations.\n"
+            "Created new persons {}.\nCreated P2C links {}."
+            .format(persons_created_total, p2c_links_total)
         )
+        # endregion
 
-    def create_person(self, person_name, is_pep, created_peps, created_assoc, real_run=False):
+        # region Create P2P connections
+        p2p_links_total = 0
+        heads = self.company_heads_mapping()
+
+        for candidate in smida_candidates.nocache().iterator():
+            edrpou = unicode(candidate.smida_edrpou).rjust(8, "0")
+            person_name = candidate.smida_parsed_name
+
+            heads_of_company = heads.get(edrpou) or []
+
+            # it is likely that either from_person or to_person is None
+            # as we skip processing names that was already have records in db
+            from_person = persons_dict.get(person_name)
+            if not from_person:
+                tqdm.write("Can not create P2P. Candidate '{}' was not recognized"
+                           " as unique person".format(person_name))
+                continue
+
+            for head in heads_of_company:
+                to_person = persons_dict.get(head)
+
+                if not to_person:
+                    tqdm.write("Can not create P2P. Head '{}' was not recognized"
+                               " as unique person".format(head))
+                    continue
+
+                p2p = Person2Person(from_person=from_person,
+                                    to_person=to_person,
+                                    from_relationship_type="ділові зв'язки",
+                                    to_relationship_type="ділові зв'язки")
+
+                tqdm.write("Created P2P relation: id: {} ({}) <=> id: {} ({})"
+                           .format(from_person.id or "N/A",
+                                   from_person.full_name,
+                                   to_person.id or "N/A",
+                                   to_person.full_name))
+                p2p_links_total += 1
+
+                if options["real_run"]:
+                    p2p.save()
+
+        self.stdout.write(
+            "Finished import Person2Person relations.\n"
+            "Total P2P links created {}."
+                .format(persons_created_total, p2p_links_total)
+        )
+        # endregion
+
+    def create_person(self, person_name, is_pep, created_persons, real_run=False):
         qs = Person.objects.all()
 
         for term in person_name.split():
@@ -133,9 +172,20 @@ class Command(BaseCommand):
                            Q(last_name_uk__icontains=term) |
                            Q(patronymic_uk__icontains=term))
 
-        if qs.nocache().count() == 0:
-            self.stdout.write("No matches for: {}. Person will be created".format(person_name))
+        name_matches = qs.nocache().count()
+
+        # As we have not enough of proof that SmidaCandidate name matched
+        # in storage is same person, he will be skipped from processing
+        # in order to avoid creating duplicate objects
+
+        if name_matches == 0:
+            tqdm.write("No matches for: {}. Person will be created"
+                       .format(person_name))
             names = person_name.split()
+
+            if len(names) < 2:
+                tqdm.write("Can not split name: {}".format(person_name))
+                return
 
             # Create new person
             person = Person(
@@ -146,18 +196,19 @@ class Command(BaseCommand):
                 type_of_official=1 if is_pep else 4
             )
 
-            # self.apply_dob(candidate.smida_yob, person)
+            # self.apply_dob(candidate.smida_yob, person) #TODO should it be applied?
 
             if real_run:
                 person.save()
 
-            if is_pep:
-                created_peps[person_name] = person
-            else:
-                created_assoc[person_name] = person
+            created_persons[person_name] = person
+            return person
 
-            return person, True
-        return None, False
+        else:
+            tqdm.write("Found {} {} for name: {}. Skip it from processing."
+                       .format(name_matches,
+                               "matches" if name_matches > 1 else "match",
+                               person_name))
 
     def company_heads_mapping(self):
         companies_heads_dict = defaultdict(list)
@@ -185,59 +236,8 @@ class Command(BaseCommand):
                     .values_list("smida_parsed_name", flat=True)
                     .distinct("smida_parsed_name"))
 
-    def all_associated_names(self, peps):
-        all_names = list(SMIDACandidate.objects.filter(status="a",
-                                                  smida_is_real_person=True)
-                    .values_list("smida_parsed_name", flat=True)
-                    .distinct("smida_parsed_name"))
-        return list(set(all_names) - set(peps))
-
     def apply_dob(self, yob, person):
         if yob and yob > 1850:
             dob = dt_parse("{}-01-01".format(yob))
             person.dob = dob
             person.dob_details = 2
-
-
-            #edrpou = unicode(candidate.smida_edrpou).rjust(8, "0")
-            # qs = Person.objects.all()
-            #
-            # for term in person_name.split():
-            #     qs = qs.filter(Q(first_name_uk__icontains=term) |
-            #                    Q(last_name_uk__icontains=term) |
-            #                    Q(patronymic_uk__icontains=term))
-            #
-            # if qs.nocache().count() == 0:
-            #     self.stdout.write("No matches for: {}. Person will be created".format(person_name))
-            #     names = person_name.split()
-            #
-            #     # Create new person
-            #     person = Person(
-            #         last_name=names[0],
-            #         first_name=names[1],
-            #         patronymic=names[2] if len(names) == 3 else "",
-            #         is_pep=is_pep,
-            #         type_of_official=1 if is_pep else 4
-            #     )
-            #
-            #     #self.apply_dob(candidate.smida_yob, person)
-            #
-            #     if options["real_run"]:
-            #         person.save()
-            #
-            #     created_peps[person_name] = person
-            #
-            #     # Create person2company
-            #
-            #     break #TODO remove
-
-            # elif len(persons) == 1:
-            #     person = persons[0]
-            #     matched_persons_count += 1
-            #     matched_persons_keys.append(candidate.smida_parsed_name)
-            #     self.stdout.write("Matched smida person: {} ==> {} {} {}.".format(
-            #         candidate.smida_parsed_name,
-            #         person.last_name, person.first_name, person.patronymic
-            #     ))
-            # else:
-            #     pass
