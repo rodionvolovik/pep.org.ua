df359196f9491cbf7fb0181cd93318174344a8e5
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2019-02-19 16:26:22 +0200

    More juice to that report

--- _snippets/broken_connections_from_declarations.py
+++ _snippets/broken_connections_from_declarations.py
@@ -1,22 +1,62 @@
 import unicodecsv
 from core.models import Person2Company, Declaration
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     with open("/tmp/outdated_links.csv", "w") as fp:
-        w = unicodecsv.DictWriter(fp, fieldnames=["person", "connection", "relation", "last_declaration"])
+        w = unicodecsv.DictWriter(
+            fp,
+            fieldnames=[
+                "person",
+                "url",
+                "connection",
+                "relation",
+                "last_declaration_for_connection",
+                "last_declaration_of_person",
+                "has_newer_declarations",
+            ],
+        )
         w.writeheader()
 
-        for p2c in Person2Company.objects.all().nocache().iterator():
+        for p2c in (
+            Person2Company.objects.select_related("from_person", "to_company")
+            .nocache()
+            .iterator()
+        ):
             if not p2c.date_finished and p2c.declarations:
                 declaration_years = Declaration.objects.filter(
-                    pk__in = p2c.declarations
+                    pk__in=p2c.declarations, confirmed="a"
                 ).values_list("year", flat=True)
                 max_year = max(map(int, declaration_years))
+
                 if max_year < 2018:
-                    w.writerow({
-                        "person": p2c.from_person,
-                        "connection": p2c.to_company,
-                        "relation": p2c.relationship_type_uk,
-                        "last_declaration": max_year
-                    })
+                    person_declaration_years = Declaration.objects.filter(
+                        person=p2c.from_person, confirmed="a"
+                    ).values_list("year", flat=True)
+
+                    person_declaration_years = list(
+                        filter(None, person_declaration_years)
+                    )
+                    if person_declaration_years:
+                        max_person_declaration_year = max(
+                            map(int, person_declaration_years)
+                        )
+                    else:
+                        max_person_declaration_year = ""
+
+                    w.writerow(
+                        {
+                            "person": p2c.from_person,
+                            "url": "https://pep.org.ua{}".format(
+                                p2c.from_person.get_absolute_url()
+                            ),
+                            "connection": p2c.to_company,
+                            "relation": p2c.relationship_type_uk,
+                            "last_declaration_for_connection": max_year,
+                            "last_declaration_of_person": max_person_declaration_year,
+                            "has_newer_declarations": (
+                                max_year != max_person_declaration_year
+                            )
+                            and bool(max_person_declaration_year),
+                        }
+                    )
