c9efcb28c06032150c334abab7dbc547aeaf8fc6
Dmitry Chaplinsky <dchaplinsky@conversionscience.co.uk>
2019-10-15 17:26:40 +0300

    Adding tqdm to reindex

--- pepdb/core/management/commands/reindex.py
+++ pepdb/core/management/commands/reindex.py
@@ -7,6 +7,7 @@ from django.conf import settings
 from elasticsearch_dsl import Index
 from elasticsearch.helpers import streaming_bulk
 from elasticsearch_dsl.connections import connections
+from tqdm import tqdm
 
 from core.models import Company, Person
 from core.elastic_models import (
@@ -34,9 +35,10 @@ class Command(BaseCommand):
         activate(settings.LANGUAGE_CODE)
         conn = connections.get_connection('default')
 
+        person_qs = Person.objects.filter(publish=True)
         docs_to_index = [
             ElasticPerson(**p.to_dict())
-            for p in Person.objects.filter(publish=True).nocache().iterator()
+            for p in tqdm(person_qs.nocache().iterator(), total=person_qs.count())
         ]
 
         if options["drop_indices"]:
@@ -61,9 +63,10 @@ class Command(BaseCommand):
             'Loaded {} persons to persistence storage'.format(
                 len(docs_to_index)))
 
+        company_qs = Company.objects.filter(publish=True)
         docs_to_index = [
             ElasticCompany(**p.to_dict())
-            for p in Company.objects.filter(publish=True).nocache().iterator()]
+            for p in tqdm(company_qs.nocache().iterator(), total=company_qs.count())]
 
         if options["drop_indices"]:
             Index(ElasticCompany._doc_type.index).delete(ignore=404)
